a1fbb4d18ee48ff7542192b2c4e3f7de506b01a3
Vladislav Lipatov <lipatov.xex@gmail.com>
2018-11-10 16:08:15 +0300

    Updates

---
+++ precache-manifest.34a42c04146643be114fddfcbed2c268.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45ca33d82e8236467700",
+    "url": "/music-app/static/js/runtime~main.45ca33d8.js"
+  },
+  {
+    "revision": "b6cb2d220c557aef6340",
+    "url": "/music-app/static/js/main.b6cb2d22.chunk.js"
+  },
+  {
+    "revision": "ed0a862898ecd5fe13fd",
+    "url": "/music-app/static/js/1.ed0a8628.chunk.js"
+  },
+  {
+    "revision": "b6cb2d220c557aef6340",
+    "url": "/music-app/static/css/main.8bdc8a10.chunk.css"
+  },
+  {
+    "revision": "377abc2e1162a06b06b9d6166e41a8e7",
+    "url": "/music-app/index.html"
+  }
+];
